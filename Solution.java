d3bb4242d5a189e5214e904947e08a20624f3db2
sdim-ua <ingersol@yandex.ua>
2019-01-24 22:52:33 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,32 @@
+/*
+
+Треугольник из восьмерок
+
+Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+        
+
+
+String str = "8"; 
+
+
+for (int i = 0; i < 10; i++ )
+        {
+    for  (int k  = 0; k <= i ; k++)
+        {
+        System.out.print(str);
+        }
+    System.out.println();
+
+        }
+   
+    }
+}
